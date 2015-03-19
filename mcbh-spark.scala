367706742b95314d537d11473207d018096feeb8
Nathan Murthy <nmurthy@solarcity.com>
2015-03-19 14:49:25 -0700

    use vals instead of REPL results

--- mcbh-spark.scala
+++ mcbh-spark.scala
@@ -1,4 +1,5 @@
-sc.textFile("file:/home/nate/Desktop/MCBH_SL1123040595.csv")
-res0.distinct.sortBy(t => t.split(",")(0))
-res1.saveAsTextFile("file:/home/nate/Desktop/MCBH_deduped")
+// replace with your local file paths
+val f = sc.textFile("file:/home/nate/Desktop/MCBH_SL1123040595.csv")
+val g = f.distinct.sortBy(t => t.split(",")(0))
+g.saveAsTextFile("file:/home/nate/Desktop/MCBH_deduped")
 
