6fb15d3774099a42b83f4a427991cf2d91bd4f6b
nmurthy-solarcity <nmurthy@solarcity.com>
2015-03-25 16:35:44 -0700

    Update mcbh-spark.scala

--- mcbh-spark.scala
+++ mcbh-spark.scala
@@ -1,5 +1,5 @@
 // replace with your local file paths
-val f = sc.textFile("file:/home/nate/Desktop/MCBH_SL1123040595.csv")
+val f = sc.textFile("file:/home/nmurthy/Desktop/MCBH_SL1123040595.csv")
 val g = f.distinct.sortBy(t => t.split(",")(0))
-g.saveAsTextFile("file:/home/nate/Desktop/MCBH_deduped")
+g.saveAsTextFile("file:/home/nmurthy/Desktop/MCBH_deduped")
 
